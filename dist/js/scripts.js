62456688032f072c671e0eb3a9b69680c8db1678
Your Name <kirillovaaru@yandex.ru>
2019-08-17 22:28:16 +0300

    Доработан Риксель Перфект на Мозиле

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -15,27 +15,27 @@ $('#offer-form').on('submit', function(event) {
     });
 });
 
-$('#offer-forms').on('submit', function(event) {
-    event.preventDefault();
-    $.ajax({
-        url: '../mail.php',
-        type: 'POST',
-        data: $(this).serialize(),
-        success: function(data) {
-            // console.log(data);
-            $('.success').html(data['username'] + ', Ваша форма отправлена, спасибо большое');
-            // $('#modalmini').modal.addClass('modalmini_active');
-        },
-        error: function(jqXHR, taxtStatus) {
-            console.log(jqXHR + ': ' + taxtStatus); 
-        }
-    });
-});
+// $('#offer-forms').on('submit', function(event) {
+//     event.preventDefault();
+//     $.ajax({
+//         url: '../mail.php',
+//         type: 'POST',
+//         data: $(this).serialize(),
+//         success: function(data) {
+//             // console.log(data);
+//             $('.success').html(data['username'] + ', Ваша форма отправлена, спасибо большое');
+//             // $('#modalmini').modal.addClass('modalmini_active');
+//         },
+//         error: function(jqXHR, taxtStatus) {
+//             console.log(jqXHR + ': ' + taxtStatus); 
+//         }
+//     });
+// });
 
 $('#offer-formms').on('submit', function(event) {
     event.preventDefault();
     $.ajax({
-        url: '../mail.php',
+        url: '../mail.php', 
         type: 'POST',
         data: $(this).serialize(),
         success: function(data) {
